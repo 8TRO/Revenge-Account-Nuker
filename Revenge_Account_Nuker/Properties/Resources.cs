92c530274d44766aff8d224a2507c62fab2f1a0b
xanthe1337 <53409082+xanthe1337@users.noreply.github.com>
2020-05-03 21:02:52 +0200

    Create Resources.cs

---
+++ Revenge_Account_Nuker/Properties/Resources.cs
@@ -0,0 +1,55 @@
+using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace Revenge_Account_Nuker.Properties
+{
+	// Token: 0x02000006 RID: 6
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class Resources
+	{
+		// Token: 0x06000011 RID: 17 RVA: 0x000027BF File Offset: 0x000009BF
+		internal Resources()
+		{
+		}
+
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				bool flag = Resources.resourceMan == null;
+				if (flag)
+				{
+					ResourceManager temp = new ResourceManager("Revenge_Account_Nuker.Properties.Resources", typeof(Resources).Assembly);
+					Resources.resourceMan = temp;
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		private static ResourceManager resourceMan;
+
+		private static CultureInfo resourceCulture;
+	}
+}

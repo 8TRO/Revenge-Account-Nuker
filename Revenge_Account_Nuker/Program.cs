0f601e347954b25de56995bd4870dbf7efbc7b22
xanthe1337 <53409082+xanthe1337@users.noreply.github.com>
2020-05-03 21:02:12 +0200

    Create Program.cs

---
+++ Revenge_Account_Nuker/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace Revenge_Account_Nuker
+{
+	internal static class Program
+	{
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
